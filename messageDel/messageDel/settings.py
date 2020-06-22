0245747739dfc5c147c8b18a98ce934e546562cc
Shubhendra Pal Singhal <shubhendrapalsinghal@gmail.com>
2020-06-22 18:07:04 +0530

    Update settings.py

--- messageDel/messageDel/settings.py
+++ messageDel/messageDel/settings.py
@@ -18,7 +18,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'shubhendrapalsinghal@gmail.com'
-EMAIL_HOST_PASSWORD = 'sha123#56sus'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
